1f423cd8b33b3459c29abcec3a22e8d4b9536461
RajeshKothiyaTrootech <rajesh.kothiya@trootech.com>
2023-12-05 15:33:27 +0530

    [IMP] variable naming, user validation and globle variable for workspace

--- odoo_setup.sh
+++ odoo_setup.sh
@@ -4,19 +4,25 @@
 #HELP: Run script on home directory
 #RUN:. odoo_setup.sh
 
-INSTALL_PYTHON3="True"
-INSTALL_POSTGRESQL="True"
-INSTALL_ANACONDA="True"
+PYTHON_INSTALL="True"
+POSTGRESQL_INSTALL="True"
+ANACONDA_INSTALL="True"
+WORKSPACE_DIR="$HOME/workspace"
 
 
 echo -e "\n----------Start----------"
 read -p "Enter odoo versions number for setup(Add version number, for multiple use space Ex:15 16):" VNAMES
 
+if ! [[ $VNAMES =~ (^|[^0-9.])(15|16|17)([^0-9.]|$) ]];then 
+    echo "Wrong input for odoo versions !"
+    return
+fi
+
 sudo apt-get update
 sudo apt-get upgrade -y
 
 
-if [ $INSTALL_PYTHON3 = "True" ]; then
+if [ $PYTHON_INSTALL = "True" ]; then
     echo -e "\n--- Installing Python 3 + pip3 --"
     sudo apt-get install git python3 python3-pip build-essential wget python3-dev python3-venv python3-wheel libxslt-dev libzip-dev libldap2-dev libsasl2-dev python3-setuptools node-less libpng12-0 libjpeg-dev gdebi -y
 else
@@ -24,7 +30,7 @@ else
 fi
 
 
-if [ $INSTALL_POSTGRESQL = "True" ]; then
+if [ $POSTGRESQL_INSTALL = "True" ]; then
     echo -e "\n---- Install PostgreSQL Server ----"
     sudo apt-get install postgresql postgresql-server-dev-all -y
 else
@@ -32,7 +38,7 @@ else
 fi
 
 
-if [ $INSTALL_ANACONDA = "True" ]; then
+if [ $ANACONDA_INSTALL = "True" ]; then
     echo -e "\n---- Install PostgreSQL Anaconda ----"
     sudo apt install curl bzip2 -y
     curl --output anaconda.sh https://repo.anaconda.com/archive/Anaconda3-5.3.1-Linux-x86_64.sh
@@ -46,19 +52,17 @@ fi
 
 
 
-mkdir workspace
-echo -e "\n====workspace====created===="
-cd workspace
+mkdir $WORKSPACE_DIR
+echo -e "\n====workspace created at $WORKSPACE_DIR"
+cd $WORKSPACE_DIR
 
 for ODOOV in $VNAMES
 do
     echo -e "\n====create==dir for odoo$ODOOV"
     mkdir odoo$ODOOV
-    cd odoo$ODOOV
-    mkdir enterprise
-    mkdir odoo
-    mkdir projects
-    cd odoo
+    cd $WORKSPACE_DIR/odoo$ODOOV
+    mkdir -p enterprise odoo projects
+    cd $WORKSPACE_DIR/odoo$ODOOV/odoo
     conda create -n env_odoo$ODOOV python=3.10
     conda activate env_odoo$ODOOV
     git clone https://www.github.com/odoo/odoo --depth 1 --branch $ODOOV.0 --single-branch .
@@ -66,5 +70,5 @@ do
     pip3 install libsass==0.22.0
     conda deactivate
     conda deactivate
-    cd ../..
+    cd $WORKSPACE_DIR
 done
